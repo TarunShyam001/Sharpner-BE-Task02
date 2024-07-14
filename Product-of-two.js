d4a64da5b97ce331f4fc7c6a19340a68f7afa752
TarunShyam001 <shyamtarun2001@gmail.com>
2024-07-14 19:32:35 +0530

    The JS code for product of two numbers are updated

---
+++ Product-of-two.js
@@ -0,0 +1,10 @@
+let valueOne = 15;
+console.log(`first number is : ${valueOne}`)
+
+let valueTwo = 27;
+console.log(`second number is : ${valueTwo}`)
+
+const productOfTwo = (a,b)=> a*b;
+
+let finalResult = productOfTwo(valueOne,valueTwo);
+console.log('finalresult of their product is : ' + finalResult);
